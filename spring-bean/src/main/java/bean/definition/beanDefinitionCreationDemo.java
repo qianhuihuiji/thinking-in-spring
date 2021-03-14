19565b0f4eea2c5c095013f8273013f9354e589d
qianhuihuiji <1025926969@qq.com>
2021-03-14 15:01:19 +0800

    32节 05:20

---
+++ spring-bean/src/main/java/bean/definition/beanDefinitionCreationDemo.java
@@ -0,0 +1,11 @@
+package bean.definition;
+
+/**
+ * {@link BeanDefinition} 构建示例
+ */
+public class beanDefinitionCreationDemo {
+
+    public static void main(String[] args) {
+        // 1.通过 BeanDefinitionBuilder 构建
+    }
+}
