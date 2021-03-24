71c56c0b0caf5fd0e2c3dd2e71f5f1bf74a0116c
qianhuihuiji <1025926969@qq.com>
2021-03-24 13:36:26 +0800

    44节 00:14

---
+++ dependency-lookup/src/main/java/lookup/ObjectProviderDemo.java
@@ -0,0 +1,35 @@
+package lookup;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * {@Link ObjectProvider } Demo
+ */
+public class ObjectProviderDemo { // @Configuration 是非必须配置
+
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 将当前类 ObjectProviderDemo 作为配置类
+        applicationContext.register(ObjectProviderDemo.class);
+        // 启动上下文
+        applicationContext.refresh();
+        // 依赖查找集合对象
+        lookupByObjectProvider(applicationContext);
+
+        // 关闭上下文
+        applicationContext.close();
+    }
+
+    @Bean
+    public String helloWord() { // 方法名就是 Bean 名称 = "helloWord"
+        return "Hello,world!";
+    }
+
+    private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        System.out.println(objectProvider.getObject());
+    }
+}
