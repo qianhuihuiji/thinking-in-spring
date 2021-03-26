544447d327a5f5e3d3f9dd603592b67deda5fe44
qianhuihuiji <1025926969@qq.com>
2021-03-26 19:21:10 +0800

    46节 00:07

--- dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/HierarchicalDependencyLookupDemo.java
+++ dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/HierarchicalDependencyLookupDemo.java
@@ -1,5 +1,10 @@
 package org.nofirst.thinking.in.spring.dependencylookup.lookup;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.HierarchicalBeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,11 +16,62 @@ public class HierarchicalDependencyLookupDemo {
         // 创建 BeanFactory 容器
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         // 将当前类 ObjectProviderDemo 作为配置类
-        applicationContext.register(HierarchicalDependencyLookupDemo.class);
+        applicationContext.register(ObjectProviderDemo.class);
+
+        // 1. 获取 HierarchicalBeanFactory <- ConfigurableBeanFactory <- ConfigurableListableBeanFactory
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+//        System.out.println("当前 BeanFactory 的 Parent BeanFactory : " + beanFactory.getParentBeanFactory());
+
+        // 2. 设置 Parent BeanFactory
+        HierarchicalBeanFactory parentBeanFactory = createParentBeanFactory();
+
+        beanFactory.setParentBeanFactory(parentBeanFactory);
+//        System.out.println("当前 BeanFactory 的 Parent BeanFactory : " + beanFactory.getParentBeanFactory());
+
+        displayContainsLocalBean(beanFactory, "user");
+        displayContainsLocalBean(parentBeanFactory, "user");
+
+        displayContainsBean(beanFactory, "user");
+        displayContainsBean(parentBeanFactory, "user");
+
         // 启动上下文
         applicationContext.refresh();
 
         // 关闭上下文
         applicationContext.close();
+    }
+
+    private static void displayContainsBean(HierarchicalBeanFactory beanFactory, String beanName) {
+        System.out.printf("当前 BeanFactory[%s] 是否包含 Bean[name : %s] : %s\n", beanFactory, beanName,
+                containsBean(beanFactory, beanName));
+    }
+
+    private static boolean containsBean(HierarchicalBeanFactory beanFactory, String beanName) {
+        BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
+        if (parentBeanFactory instanceof HierarchicalBeanFactory) {
+            HierarchicalBeanFactory parentHierarchicalBeanFactory = HierarchicalBeanFactory.class.cast(parentBeanFactory);
+            if (containsBean(parentHierarchicalBeanFactory, beanName)) {
+                return true;
+            }
+        }
+        // 走到这里已经找到 root 级别的、最顶层的 beanFactory,所以判断 containsLocalBean 即可
+        return beanFactory.containsLocalBean(beanName);
+    }
+
+    private static void displayContainsLocalBean(HierarchicalBeanFactory beanFactory, String beanName) {
+        System.out.printf("当前 BeanFactory[%s] 是否包含 Local Bean[name : %s] : %s\n", beanFactory, beanName,
+                beanFactory.containsLocalBean(beanName));
+    }
+
+    private static ConfigurableListableBeanFactory createParentBeanFactory() {
+        // 创建 BeanFactory 容器
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // Xml 配置文件 ClassPath 路径
+        String location = "classpath:/META-INF/dependency-lookup-context.xml";
+        // 加载配置
+        reader.loadBeanDefinitions(location);
+
+        return beanFactory;
     }
 }
