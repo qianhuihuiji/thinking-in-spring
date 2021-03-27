6a45421bfad13fe5c46c372ab17af20a3035ae2e
qianhuihuiji <1025926969@qq.com>
2021-03-27 22:51:49 +0800

    47节 00:01

--- dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/ObjectProviderDemo.java
@@ -1,8 +1,11 @@
 package org.nofirst.thinking.in.spring.dependencylookup.lookup;
 
+import java.util.Iterator;
+import org.nofirst.thinking.in.spring.iocoverview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * {@Link ObjectProvider } Demo
@@ -18,14 +21,37 @@ public class ObjectProviderDemo { // @Configuration 是非必须配置
         applicationContext.refresh();
         // 依赖查找集合对象
         lookupByObjectProvider(applicationContext);
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
 
         // 关闭上下文
         applicationContext.close();
     }
 
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+//        Iterable<String> stringIterable = objectProvider;
+//        for (String string : stringIterable) {
+//            System.out.println(string);
+//        }
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前对象：" + user);
+    }
+
     @Bean
+    @Primary
     public String helloWord() { // 方法名就是 Bean 名称 = "helloWord"
         return "Hello,world!";
+    }
+
+    @Bean
+    public String message() { // 方法名就是 Bean 名称 = "helloWord"
+        return "message!";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
