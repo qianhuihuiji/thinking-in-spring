58ac5cd971ca9f15877ba15c4af8d928e80ac844
qianhuihuiji <1025926969@qq.com>
2021-03-28 20:40:45 +0800

    48节 01:01

--- dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/TypeSafetyDependencyLookupDemo.java
+++ dependency-lookup/src/main/java/org/nofirst/thinking/in/spring/dependencylookup/lookup/TypeSafetyDependencyLookupDemo.java
@@ -3,7 +3,9 @@ package org.nofirst.thinking.in.spring.dependencylookup.lookup;
 import org.nofirst.thinking.in.spring.iocoverview.domain.User;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -23,15 +25,36 @@ public class TypeSafetyDependencyLookupDemo {
         displayBeanFactoryGetBean(applicationContext);
         // 演示 ObjectFactory#getObject 方法的安全性
         displayObjectFactoryGetObject(applicationContext);
+        // 演示 ObjectProvider#getIfAvailable 方法的安全性
+        displayObjectProviderGetIfAvailable(applicationContext);
+
+        // 演示 ListableBeanFactory#getBeansOfType 方法的安全性
+        displayListableBeanFactoryGetBeansOfType(applicationContext);
+        // 演示 ObjectProvider Stream 操作的安全性
+        displayObjectProviderStreamOps(applicationContext);
 
         // 关闭上下文
         applicationContext.close();
     }
 
+    private static void displayObjectProviderStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        printBeansException("displayObjectProviderStreamOps", () -> userObjectProvider.forEach(System.out::println));
+    }
+
+    private static void displayListableBeanFactoryGetBeansOfType(ListableBeanFactory beanFactory) {
+        printBeansException("displayListableBeanFactoryGetBeansOfType", () -> beanFactory.getBeansOfType(User.class));
+    }
+
+    private static void displayObjectProviderGetIfAvailable(BeanFactory beanFactory) {
+        ObjectProvider<User> userObjectProvider = beanFactory.getBeanProvider(User.class);
+        printBeansException("displayObjectProviderGetIfAvailable", () -> userObjectProvider.getIfAvailable());
+    }
+
     private static void displayObjectFactoryGetObject(BeanFactory beanFactory) {
         // ObjectProvider is ObjectFactory
-        ObjectFactory<User> userObjectFactory = beanFactory.getBeanProvider(User.class);
-        printBeansException("displayObjectFactoryGetObject", () -> userObjectFactory.getObject());
+        ObjectFactory<User> userObjectProvider = beanFactory.getBeanProvider(User.class);
+        printBeansException("displayObjectFactoryGetObject", () -> userObjectProvider.getObject());
     }
 
     private static void displayBeanFactoryGetBean(BeanFactory beanFactory) {
@@ -39,11 +62,13 @@ public class TypeSafetyDependencyLookupDemo {
     }
 
     private static void printBeansException(String source, Runnable runnable) {
+        System.err.println("============== Start ============");
         System.err.println("Source from: " + source);
         try {
             runnable.run();
         }catch (BeansException e) {
             e.printStackTrace();
         }
+    System.err.println("============== End ============");
     }
 }
