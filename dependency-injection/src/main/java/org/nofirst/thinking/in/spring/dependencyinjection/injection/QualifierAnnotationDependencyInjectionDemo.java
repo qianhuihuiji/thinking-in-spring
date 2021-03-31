a9366c2db1ae854aa00bc7fe861340968f398faf
qianhuihuiji <1025926969@qq.com>
2021-03-31 23:51:12 +0800

    64节 00:01

--- dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/QualifierAnnotationDependencyInjectionDemo.java
+++ dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/QualifierAnnotationDependencyInjectionDemo.java
@@ -1,6 +1,7 @@
 package org.nofirst.thinking.in.spring.dependencyinjection.injection;
 
 import java.util.Collection;
+import org.nofirst.thinking.in.spring.dependencyinjection.injection.annotation.UserGroup;
 import org.nofirst.thinking.in.spring.iocoverview.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -35,20 +36,37 @@ public class QualifierAnnotationDependencyInjectionDemo {
     @Qualifier
     private Collection<User> qualifiedUsers; // 2 beans = user1 + user2;
 
+    @Autowired
+    @UserGroup
+    private Collection<User> groupedUsers; // 2 beans = user3 + user4;
+
     @Bean
     @Qualifier // 进行逻辑分组
     public static User user1() { // 加上 static 会让 Bean 提前初始化，如果不加的话，框架默认先加载 XML 配置中注册的 Bean，导致 allUsers 找不到配置类中的 user1 user2 这两个 Bean
-        User user = new User();
-        user.setId(7L);
-
-        return user;
+        return creatUser(7L);
     }
 
     @Bean
     @Qualifier // 进行逻辑分组
     public static User user2() {
+        return creatUser(8L);
+    }
+
+    @Bean
+    @UserGroup
+    public static User user3() {
+        return creatUser(8L);
+    }
+
+    @Bean
+    @UserGroup
+    public static User user4() {
+        return creatUser(8L);
+    }
+
+    private static User creatUser(long l) {
         User user = new User();
-        user.setId(8L);
+        user.setId(l);
 
         return user;
     }
@@ -79,6 +97,8 @@ public class QualifierAnnotationDependencyInjectionDemo {
         System.out.println("demo.allUsers: " + demo.allUsers);
         // 期待输出 user1 user2
         System.out.println("demo.qualifiedUsers: " + demo.qualifiedUsers);
+        // 期待输出 user3 user4
+        System.out.println("demo.groupedUsers: " + demo.groupedUsers);
 
         // 关闭上下文
         applicationContext.close();
