d5c29aa0a732500c0c126fbb0551f503c6e93625
qianhuihuiji <1025926969@qq.com>
2021-04-04 17:14:17 +0800

    66节 00:01

--- dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/AnnotationDependencyInjectionResolutionDemo.java
+++ dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/AnnotationDependencyInjectionResolutionDemo.java
@@ -1,5 +1,7 @@
 package org.nofirst.thinking.in.spring.dependencyinjection.injection;
 
+import java.util.Map;
+import java.util.Optional;
 import org.nofirst.thinking.in.spring.iocoverview.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -8,17 +10,36 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * 注解驱动的依赖助理过程
  *
+ * DefaultListableBeanFactory#resolveDependency
+ * 1.判断是否懒加载-->doResolveDependency()
+ * 2.判断是否是多类型的bean resolveMultipleBeans()
+ * 3.根据类型查找匹配到的bean findAutowireCandidates()
+ * 4.bean个数大于1,选择bean即@Primary修饰 determineAutowireCandidate()
+ * 返回结果
+ *
  * @date: 2021/03/29
  **/
 public class AnnotationDependencyInjectionResolutionDemo {
 
-    @Autowired         // 依赖查找（处理）
-    private User user; // DependencyDescriptor -->
-                       // 必须的（required = true）
-                       // 是否首要（primary = true）
-                       // 实时注入（eager = true）
-                       // 通过类型（User.class）
-                       // 字段名称（"user"）
+    // 依赖查找（处理）+ 延迟
+    @Autowired
+    private User lazyUser;
+
+    // 依赖查找（处理）
+    // DependencyDescriptor -->
+    // 必须的（required = true）
+    // 是否首要（primary = true）
+    // 实时注入（eager = true）
+    // 通过类型（User.class）
+    // 字段名称（"user"）
+    @Autowired
+    private User user;
+
+    @Autowired
+
+    private Map<String, User> users;
+    @Autowired
+    private Optional<User> optionalUser;
 
     public static void main(String[] args) {
         // 创建 BeanFactory 容器
@@ -40,6 +61,10 @@ public class AnnotationDependencyInjectionResolutionDemo {
 
         // 期待输出 superUser
         System.out.println("demo.user: " + demo.user);
+        // 期待输出 user superUser （先注册的 bean 先初始化）
+        System.out.println("demo.users: " + demo.users);
+        // 期待输出 superUser
+        System.out.println("demo.optionalUser: " + demo.optionalUser);
 
         // 关闭上下文
         applicationContext.close();
