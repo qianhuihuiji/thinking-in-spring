5d2d0d6155fc3b59934e215cee1762fe2e3f74b5
qianhuihuiji <1025926969@qq.com>
2021-04-05 23:31:31 +0800

    68节 00:01

--- dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/AnnotationDependencyInjectionResolutionDemo.java
+++ dependency-injection/src/main/java/org/nofirst/thinking/in/spring/dependencyinjection/injection/AnnotationDependencyInjectionResolutionDemo.java
@@ -2,6 +2,7 @@ package org.nofirst.thinking.in.spring.dependencyinjection.injection;
 
 import java.util.Map;
 import java.util.Optional;
+import javax.inject.Inject;
 import org.nofirst.thinking.in.spring.iocoverview.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -16,6 +17,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * 3.根据类型查找匹配到的bean findAutowireCandidates()
  * 4.bean个数大于1,选择bean即@Primary修饰 determineAutowireCandidate()
  * 返回结果
+ *
+ * AutowiredAnnotationBeanPostProcessor 可以处理 @Autowired、@Value 和 @Inject 注解
  *
  * @date: 2021/03/29
  **/
@@ -36,10 +39,13 @@ public class AnnotationDependencyInjectionResolutionDemo {
     private User user;
 
     @Autowired
-
     private Map<String, User> users;
+
     @Autowired
     private Optional<User> optionalUser;
+
+    @Inject
+    private User injectedUser;
 
     public static void main(String[] args) {
         // 创建 BeanFactory 容器
@@ -61,6 +67,9 @@ public class AnnotationDependencyInjectionResolutionDemo {
 
         // 期待输出 superUser
         System.out.println("demo.user: " + demo.user);
+        System.out.println("demo.injectedUser: " + demo.injectedUser);
+        System.out.println("demo.user == demo.injectedUser: " + (demo.user == demo.injectedUser));
+
         // 期待输出 user superUser （先注册的 bean 先初始化）
         System.out.println("demo.users: " + demo.users);
         // 期待输出 superUser
