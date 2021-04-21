310e94f24d91dcd3d179428f2293a0cdba413bf8
qianhuihuiji <1025926969@qq.com>
2021-04-21 20:03:14 +0800

    94节 00:01

--- bean-lifecycle/src/main/java/org/nofirst/thinking/in/spring/bean/lifecycle/BeanInstantiationLifecycleDemo.java
+++ bean-lifecycle/src/main/java/org/nofirst/thinking/in/spring/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -1,11 +1,15 @@
 package org.nofirst.thinking.in.spring.bean.lifecycle;
 
+import org.nofirst.thinking.in.spring.iocoverview.domain.SuperUser;
 import org.nofirst.thinking.in.spring.iocoverview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
+import org.springframework.util.ObjectUtils;
 
 /**
  * Bean 实例化生命周期示例
@@ -14,6 +18,7 @@ public class BeanInstantiationLifecycleDemo {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
         // 基于 XML 资源 BeanDefinitionReader 实现
         XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
         String location = "META-INF/dependency-lookup-context.xml";
@@ -31,5 +36,18 @@ public class BeanInstantiationLifecycleDemo {
 
         User superUser = beanFactory.getBean("superUser", User.class);
         System.out.println(superUser );
+    }
+
+    static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+        @Override
+        public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+            if (ObjectUtils.nullSafeEquals("superUser", beanName) && SuperUser.class.equals(beanClass)) {
+                // 把配置的 superUser Bean 覆盖
+                return new SuperUser();
+            }
+
+            return null; // 保持 Spring IoC 容器的实例化操作
+        }
     }
 }
